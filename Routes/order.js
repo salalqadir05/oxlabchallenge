a6b76af3b6c04f62b126334e4dc30b1816ddf8eb
Muhammad Salal Qadir <salalqadir05@gmail.com>
2024-11-10 09:27:20 -0800

    update the file order file

--- Routes/order.js
+++ Routes/order.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { add_order } = require('../Controller/order');
 
 
-router.post('addorder', add_order);
+router.post('/addorder', add_order);
 
 module.exports = router;
